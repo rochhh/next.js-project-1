d7420bb0430be0fcbd50fcbb470a6d839ad14e7c
Roch <rochishnu7@gmail.com>
2023-06-12 01:00:58 +0530

    Initial setup done

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,22 @@
-import './globals.css'
+import React, { Children } from 'react'
+import Nav from '@/components/Nav'
+import Provider from '@/components/Provider'
+export const metadata = {
+    title: "prompt title ",
+    description : "this is a description"
+} 
 
-export default function RootLayout({ children }) {
+const RootLayout = ({children}) => {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <html lang='en' >
+        <body>
+            <main>
+              <Nav />
+                {children}
+            </main>
+        </body>
     </html>
   )
 }
+
+export default RootLayout
---
+++ components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { signIn , signOut , useSession , getProviders } from 'next-auth/react'
+
+const Nav = () => {
+  return (
+    <nav>Nav</nav>
+  )
+}
+
+export default Nav
